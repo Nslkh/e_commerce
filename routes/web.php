897a2aa1cf8e0f762541d4d1cf997feb2326e499
Nslkh <olluzran@gmail.com>
2021-06-02 09:47:37 +0500

    extending layouts

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('admin.dashboard');
 });
+
+Route::get('/index/test', function () {
+    return view('test');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
